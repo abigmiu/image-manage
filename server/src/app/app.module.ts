3f69614640edaf343d4c22483123a9fb123160bf
uranus <abigmiu@uranusdeMBP.lan>
2023-08-18 23:26:48 +0800

    init response format

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TransformInterceptor } from 'src/interceptors/transfrom.interceptor';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [],
+    controllers: [AppController],
+    providers: [
+        AppService,
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: TransformInterceptor
+        }
+    ],
 })
 export class AppModule {}
---
+++ server/src/interceptors/transfrom.interceptor.ts
@@ -0,0 +1,39 @@
+// response transfrom
+import type { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import type { Request, Response } from 'express';
+import type { Observable } from 'rxjs';
+
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { map } from 'rxjs/operators';
+
+
+
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+    constructor() { }
+
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        const request = context.switchToHttp().getRequest<Request>();
+        const response = context.switchToHttp().getResponse<Response>();
+        if (request.method.toUpperCase() === 'POST' && response.statusCode === HttpStatus.CREATED) {
+            response.status(HttpStatus.OK);
+        }
+        if (request.method.toUpperCase() === 'GET' && response.statusCode == HttpStatus.NOT_MODIFIED) {
+            response.status(HttpStatus.OK);
+        }
+
+        return next.handle().pipe(
+            map((data) => {
+                const ret = {
+                    code: HttpStatus.OK,
+                    data: data || null,
+                    msg: 'ok',
+                };
+
+
+                return ret;
+            }),
+        );
+    }
+}
