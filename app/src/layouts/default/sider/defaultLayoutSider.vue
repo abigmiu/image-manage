80661a53e208e8071f3e6211ab37fff762576725
tangying <tangying@ganguo.hk>
2023-08-29 11:57:08 +0800

    路由切换

--- app/src/layouts/default/sider/defaultLayoutSider.vue
+++ app/src/layouts/default/sider/defaultLayoutSider.vue
@@ -1,36 +1,40 @@
 <template>
-    <NMenu :options="menuOptions" class="default-layout--sider" inverted>
+    <NMenu 
+        :options="menuOptions"
+         class="default-layout--sider"
+        inverted
+        @update-value="onClickMenuItem"    
+    >
 
     </NMenu>
 </template>
 <script setup lang="ts">
 import type { MenuOption } from 'naive-ui'
 import { NMenu } from 'naive-ui'
+import { useRouter } from 'vue-router';
 
-const menuOptions: MenuOption[] = [
-    {
-        label: '图片管理',
-        key: 'image-manage',
-        children: [
-            {
-                label: '上传图片',
-                key: 'image-manage--upload',
-            },
-            {
-                label: '图片列表',
-                key: 'image-manage--list'
+const router = useRouter();
+const routes = router.options.routes[0].children;
+
+const menuOptions: MenuOption[] = []
+
+routes?.forEach(route => {
+    menuOptions.push({
+        label: route.meta!.title,
+        key: route.name as string,
+        children: (route.children || []).map((childRoute) => {
+            console.log(childRoute);
+            return {
+                label: childRoute.meta!.title,
+                key: childRoute.name as string
             }
-        ]
-    },
-    {
-        label: '标签管理',
-        key: 'tags-manage',
-        children: [
-            {
-                label: '标签列表',
-                key: 'tags-manage-list'
-            }
-        ]
-    }
-]
+        })
+    })
+})
+
+function onClickMenuItem(key: string) {
+    router.push({
+        name: key
+    })
+}
 </script>
--- app/src/router/routes/index.ts
+++ app/src/router/routes/index.ts
@@ -1,22 +1,59 @@
 import { RouteRecordRaw } from 'vue-router'
+import defaultLayout from '@/layouts/default/defaultLayout.vue'
+import App from '@/App.vue'
 
 export const route: RouteRecordRaw[] = [
     {
         path: '/',
-        component: () => import('@/layouts/default/defaultLayout.vue'),
+        component: App,
         children: [
             {
-                path: 'image-list',
-                component: () => import('@/views/image/imageList.vue')
+                path: '/image',
+                component: defaultLayout,
+                name: 'image',
+                meta: {
+                    title: '图片管理',
+                },
+                children: [
+                    {
+                        path: 'list',
+                        name: 'imageList',
+                        meta: {
+                            title: '图片列表',
+
+                        },
+                        component: () => import('@/views/image/imageList.vue')
+                    },
+                    {
+                        path: 'create',
+                        name: 'imageCreate',
+                        meta: {
+                            title: '创建图片',
+
+                        },
+                        component: () => import('@/views/image/imageUpload.vue')
+                    }
+                ]
             },
             {
-                path: 'image-create',
-                component: () => import('@/views/image/imageUpload.vue')
-            },
-            {
-                path: 'tags-list',
-                component: () => import('@/views/tags/tagsList.vue')
+                path: '/tags',
+                component: defaultLayout,
+                name: 'tags',
+                meta: {
+                    title: '标签管理',
+                },
+                children: [
+                    {
+                        path: 'list',
+                        name: 'tagsList',
+                        meta: {
+                            title: '标签列表',
+                        },
+                        component: () => import('@/views/tags/tagsList.vue')
+                    }
+                ]
             }
         ]
-    }
+    },
+
 ]
